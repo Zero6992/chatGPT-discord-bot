018f20d1959dd572c7b3f9db795f921b53c3a567
Mirtris <mirtris0522@gmail.com>
2022-12-05 23:41:52 +0800

    feat: email and password auth

--- responses.py
+++ responses.py
@@ -5,17 +5,18 @@ with open('config.json', 'r') as f:
     data = json.load(f)
 
 config = {
-    "Authorization": data['Authorization'],
-    "session_token": data['session_token']
+    "email": data['email'],
+    "password": data['password'],
 }
 
+if data['session_token']:
+    config.update(session_token = data['session_token'])
 
 chatbot = Chatbot(config, conversation_id=None)
-chatbot.refresh_session() 
+chatbot.refresh_session()
 
-def handle_response(message) -> str:
-    prompt = message
-    response = chatbot.get_chat_response(prompt)
-    responseMessage = response['message']
+def handle_response(prompt) -> str:
+    response = chatbot.get_chat_response(prompt, output="text")
+    responseMessage = response["message"]
 
     return responseMessage
