d1e215187499ddf513b1dc7818e050a72d94ab2a
aW3ikun <23023918+aW3ikun@users.noreply.github.com>
2022-12-07 06:00:59 +0800

    split user_message and modify instructions
    splitting user_message using space, modifying instructions to more explicit, and altering the bot.send_message function.

--- src/bot.py
+++ src/bot.py
@@ -5,18 +5,24 @@ from src import responses
 with open('config.json', 'r') as f:
     data = json.load(f)
 
-async def send_message(message, user_message, is_private):
+
+# the common interface to send message
+async def send_message(message, user_message, is_private=False, is_reply=True):
     try:
-        response = responses.handle_response(user_message)
-        if len(response) > 2000:
-            # Split the response into smaller chunks of no more than 2000 characters each
-            response_chunks = [response[i:i+2000]
-                               for i in range(0, len(response), 2000)]
-            for chunk in response_chunks:
-                # Send each chunk separately
-                await message.author.send(chunk) if is_private else await message.channel.send(chunk)
+        if (is_reply):
+            response = responses.handle_response(user_message)
+            if len(response) > 2000:
+                # Split the response into smaller chunks of no more than 2000 characters each
+                response_chunks = [response[i:i+2000]
+                                   for i in range(0, len(response), 2000)]
+                for chunk in response_chunks:
+                    # Send each chunk separately
+                    await message.author.send(chunk) if is_private else await message.channel.send(chunk)
+            else:
+                await message.author.send(response) if is_private else await message.channel.send(response)
         else:
-            await message.author.send(response) if is_private else await message.channel.send(response)
+            # directly send message
+            await message.author.send(user_message)
 
     except Exception as e:
         await message.channel.send("**Error: I think there are something went wrong. Please try again later!**")
@@ -24,6 +30,7 @@ async def send_message(message, user_message, is_private):
 
 intents = discord.Intents.default()
 intents.message_content = True
+
 
 def run_discord_bot():
     TOKEN = data['discord_bot_token']
@@ -43,15 +50,22 @@ def run_discord_bot():
         channel = str(message.channel)
 
         print(f"{username} said: '{user_message}' ({channel})")
+        # spilt user_message, using space
+        user_message = user_message.split(' ', 1)
 
-        if user_message == '!reset':
+        if user_message[0] == '!chat_reset':
             responses.chatbot.reset_chat()
-            await message.channel.send("**Info: I have forgotten everything.**")
             print("The CHAT BOT has been successfully reset")
-        elif user_message[0] == '!':
-            user_message = user_message[1:]
+            await send_message(message, "**Info: I have forgotten everything.**", is_reply=False)
+        # send DM to a user
+        elif user_message[0] == '!chat_private':
+            user_message = user_message[1]
             await send_message(message, user_message, is_private=True)
-        else:
+        # send messages to a chatroom
+        elif user_message[0] == '!chat':
+            user_message = user_message[1]
             await send_message(message, user_message, is_private=False)
+        else:
+            return
 
     client.run(TOKEN)
