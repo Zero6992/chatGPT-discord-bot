847627e5fa908f972fce33d8cf28645850c477bd
mirtris <mirtris0522@gmail.com>
2022-12-07 18:36:52 +0800

    feat: refactor the code

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,8 @@
 import discord
 import json
+from discord.ext import commands
 from src import responses
+
 
 with open('config.json', 'r') as f:
     data = json.load(f)
@@ -24,34 +26,54 @@ async def send_message(message, user_message, is_private):
 
 intents = discord.Intents.default()
 intents.message_content = True
+is_private = False
 
 def run_discord_bot():
     TOKEN = data['discord_bot_token']
-    client = discord.Client(intents=intents)
-
+    client = commands.Bot(command_prefix='/', intents=intents)
+    
     @client.event
     async def on_ready():
         print(f'{client.user} is now running!')
 
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
+    @client.command()
+    async def chat(ctx, *, message: str):
+        if ctx.author == client.user:
             return
-
-        username = str(message.author)
-        user_message = str(message.content)
-        channel = str(message.channel)
-
+        username = str(ctx.author)
+        user_message = message
+        channel = str(ctx.channel)
         print(f"{username} said: '{user_message}' ({channel})")
+        await send_message(ctx, user_message, is_private)
+        
+    @client.command()
+    async def private(ctx):
+            global is_private
+            if not is_private:
+                is_private = not is_private
+                print("Switch to private mode")
+                await ctx.channel.send("**Info: Next, the response will be sent via private message. If you want to switch back to public mode, use `/public`**")
+            else:
+                print("You already on private mode!")
+                await ctx.channel.send("**Warn: You already on private mode. If you want to switch to public mode, use `/public`**")
 
-        if user_message == '!reset':
-            responses.chatbot.reset_chat()
-            await message.channel.send("**Info: I have forgotten everything.**")
-            print("The CHAT BOT has been successfully reset")
-        elif user_message[0] == '!':
-            user_message = user_message[1:]
-            await send_message(message, user_message, is_private=True)
-        else:
-            await send_message(message, user_message, is_private=False)
+
+    @client.command()
+    async def public(ctx):
+            global is_private
+            if is_private:
+                is_private =  not is_private
+                print("Switch to public mode")
+                await ctx.channel.send("**Info: Next, the response will be sent to the channel directly. If you want to switch back to private mode, use `/private`**")
+            else:
+                print("You already on public mode!")
+                await ctx.channel.send("**Warn: You already on public mode. If you want to switch to private mode, use `/private`**")            
+
+    @client.command()
+    async def reset(ctx):
+        responses.chatbot.reset_chat()
+        await ctx.channel.send("**Info: I have forgotten everything.**")
+        print("The CHAT BOT has been successfully reset")
+
 
     client.run(TOKEN)
