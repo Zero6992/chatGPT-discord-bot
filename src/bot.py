77aa1acdbe648206441115c665779d1c239e0c3d
Mirtris <mirtris0522@gmail.com>
2022-12-08 04:03:56 +0800

    feat: handle ephemeral response

--- src/bot.py
+++ src/bot.py
@@ -8,29 +8,19 @@ from src import responses
 with open('config.json', 'r') as f:
     data = json.load(f)
 
-async def send_message(message, user_message, is_private):
+async def send_message(message, user_message):
     try:
         response = '> **' + user_message + '** - <@' + str(message.user.id)  + '>\n\n' + responses.handle_response(user_message)
-        if len(response) > 2000:
-            # Split the response into smaller chunks of no more than 2000 characters each
-            response_chunks = [response[i:i+2000]
-                               for i in range(0, len(response), 2000)]
+        if len(response) > 1900:
+            # Split the response into smaller chunks of no more than 1900 characters each(Discord limit is 2000 per chunk)
+            response_chunks = [response[i:i+1900]
+                               for i in range(0, len(response), 1900)]
             for chunk in response_chunks:
-                if is_private:
-                    await message.followup.send(chunk)
-                else:
-                    await message.channel.send(chunk)
-                    await message.followup.send(' :tada: :tada: The reply to this message has been sent')
+                await message.followup.send(chunk)
         else:
-            if is_private:
-                await message.followup.send(response)
-            else:
-                await message.channel.send(response)
-                await message.followup.send(' :tada: :tada: The reply to this message has been sent')
-
+            await message.followup.send(response)
     except Exception as e:
-        await message.followup.send('I made a mistake, please ask me again later')
-        await message.channel.send("> **Error: There are something went wrong. Please try again later!**")
+        await message.followup.send("> **Error: There are something went wrong. Please try again later!**")
         print(e)
 
 intents = discord.Intents.default()
@@ -54,42 +44,39 @@ def run_discord_bot():
         user_message = message
         channel = str(interaction.channel)
         print(f"{username} said: '{user_message}' ({channel})")
-        await interaction.response.defer(ephemeral = True)
-        await send_message(interaction, user_message, is_private)
+        await interaction.response.defer(ephemeral = is_private)
+        await send_message(interaction, user_message)
         
     @client.tree.command(name="private", description="Toggle private access")
     async def private(interaction: discord.Interaction):
             global is_private
-            await interaction.response.defer(ephemeral = True)
+            await interaction.response.defer(ephemeral = False)
             if not is_private:
                 is_private = not is_private
                 print("Switch to private mode")
-                await interaction.followup.send("**Info: Next, the response will be sent via private message. If you want to switch back to public mode, use `/public`**")
-                await interaction.channel.send("> **Info: Next, the response will be sent via private message. If you want to switch back to public mode, use `/public`**")
+                await interaction.followup.send("> **Info: Next, the response will be sent via private message. If you want to switch back to public mode, use `/public`**")
             else:
                 print("You already on private mode!")
-                await interaction.followup.send("**Warn: You already on private mode. If you want to switch to public mode, use `/public`**")
+                await interaction.followup.send("> **Warn: You already on private mode. If you want to switch to public mode, use `/public`**")
 
 
     @client.tree.command(name="public", description="Toggle public access")
     async def public(interaction: discord.Interaction):
             global is_private
-            await interaction.response.defer(ephemeral = True)
+            await interaction.response.defer(ephemeral = False)
             if is_private:
                 is_private =  not is_private
+                await interaction.followup.send("> **Info: Next, the response will be sent to the channel directly. If you want to switch back to private mode, use `/private`**")
                 print("Switch to public mode")
-                await interaction.followup.send("**Info: Next, the response will be sent to the channel directly. If you want to switch back to private mode, use `/private`**")
-                await interaction.channel.send("> **Info: Next, the response will be sent to the channel directly. If you want to switch back to private mode, use `/private`**")
             else:
+                await interaction.followup.send("> **Warn: You already on public mode. If you want to switch to private mode, use `/private`**")            
                 print("You already on public mode!")
-                await interaction.followup.send("**Warn: You already on public mode. If you want to switch to private mode, use `/private`**")            
 
     @client.tree.command(name="reset", description="Complete reset gptChat conversation history")
     async def reset(interaction: discord.Interaction):
         responses.chatbot.reset_chat()
-        await interaction.response.defer(ephemeral = True)
-        await interaction.followup.send("**Info: I have forgotten everything.**")
-        await interaction.channel.send("> **Info: I have forgotten everything.**")
+        await interaction.response.defer(ephemeral = False)
+        await interaction.followup.send("> **Info: I have forgotten everything.**")
         print("The CHAT BOT has been successfully reset")
 
 
