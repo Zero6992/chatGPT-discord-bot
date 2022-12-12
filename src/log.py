26c483abc7763cee7894c81d02856746c9b3967c
aW3ikun <23023918+aW3ikun@users.noreply.github.com>
2022-12-12 16:32:30 +0800

    fix a comment

--- src/log.py
+++ src/log.py
@@ -46,7 +46,7 @@ def setup_logger(module_name:str) -> logging.Logger:
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
     console_handler.setFormatter(CustomFormatter())
-     # specify log file path as `main.py`
+     # specify that the log file path is the same as `main.py` file path
     grandparent_dir = os.path.abspath(__file__ + "/../../")
     log_name='chatgpt_discord_bot.log'
     log_path = os.path.join(grandparent_dir, log_name)
