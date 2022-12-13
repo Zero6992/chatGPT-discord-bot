1ae663d7ca9ddc6bb83522793153aed660c81bd7
Jacky Chay <68509767+jacky-chay@users.noreply.github.com>
2022-12-13 17:23:35 +0800

    update on checking dependency version

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 from src import bot
 import subprocess
 import os
+import sys
 
-if __name__ == '__main__':
-    if "DOCKER_HOST" in os.environ:
-        print("Environment is Docker")
-        print("Installing requirementsDocker.txt")
-        subprocess.run(["pip", "install", "-r", "requirementsDocker.txt"])
-    else:
-        print("Environment is not Docker")
-        print("Installing requirements.txt")
-        subprocess.run(["pip", "install", "-r", "requirements.txt"])
-    
+def check_verion() -> None:
+    import pkg_resources
+    import src.log
+
+    # init loggger
+    logger = src.log.setup_logger(__name__)
+
+    # Read the requirements.txt file and add each line to a list
+    with open('requirements.txt') as f:
+        required = f.read().splitlines()
+
+    # For each library listed in requirements.txt, check if the corresponding version is installed
+    for package in required:
+        # Use the pkg_resources library to get information about the installed version of the library
+        package_name, package_verion = package.split('==')
+        installed = pkg_resources.get_distribution(package_name)
+        # Extract the library name and version number
+        name, version = installed.project_name, installed.version
+        # Compare the version number to see if it matches the one in requirements.txt
+        if package != f'{name}=={version}':
+            logger.exception(f'{name} version {version} is installed but does not match the requirements')
+            sys.exit();
+
+if __name__ == '__main__': 
+    check_verion()
     bot.run_discord_bot()
