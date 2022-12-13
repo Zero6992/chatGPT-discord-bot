949c6342ae510a1dab4781357ab1525e7071ad5d
Jacky Chay <68509767+jacky-chay@users.noreply.github.com>
2022-12-13 11:41:03 +0800

    added requirements for docker

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ FROM python:3.9.15-slim-bullseye
 COPY ./ /DiscordBot
 WORKDIR /DiscordBot
 
-RUN pip install -r requirements.txt
+RUN pip install -r requirementsDocker.txt
 
 CMD ["python3", "-u", "main.py"]
--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from src import bot
+import subprocess
+import os
 
 if __name__ == '__main__':
+    if "DOCKER_HOST" in os.environ:
+        print("Environment is Docker")
+        print("Installing requirementsDocker.txt")
+        subprocess.run(["pip", "install", "-r", "requirementsDocker.txt"])
+    else:
+        print("Environment is not Docker")
+        print("Installing requirements.txt")
+        subprocess.run(["pip", "install", "-r", "requirements.txt"])
+    
     bot.run_discord_bot()
