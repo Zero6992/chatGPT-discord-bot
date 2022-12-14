b185e75bd1337d609eb6a43ae8d7d4156e570244
Mirtris <mirtris0522@gmail.com>
2022-12-15 04:38:13 +0800

    feat: update help command

--- src/bot.py
+++ src/bot.py
@@ -150,7 +150,7 @@ def run_discord_bot():
     @client.tree.command(name="help", description="Show help for the bot")
     async def help(interaction: discord.Interaction):
         await interaction.response.defer(ephemeral=False)
-        await interaction.followup.send(":star:**BASIC COMMANDS** \n   `/chat [message]` Chat with ChatGPT!\n   `/reset` ChatGPT conversation history will be erased \n   `/private` ChatGPT switch to private mode \n   `/public` ChatGPT switch to public mode")
+        await interaction.followup.send(":star:**BASIC COMMANDS** \n   `/chat [message]` Chat with ChatGPT!\n   `/reset` ChatGPT conversation history will be erased \n   `/private` ChatGPT switch to private mode \n   `/public` ChatGPT switch to public mode \n   For complete documentation, please visit https://github.com/Zero6992/chatGPT-discord-bot")
         logger.info(
             "\x1b[31mSomeone need help!\x1b[0m")
 
