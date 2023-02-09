6589efe428d3be977d16d2eefe4d45aeb93c512c
mirtris <mirtris0522@gmail.com>
2023-02-09 15:00:45 +0800

    feat: support env

--- src/responses.py
+++ src/responses.py
@@ -1,23 +1,11 @@
 from revChatGPT.Official import AsyncChatbot
 import json
 
-def get_config() -> dict:
-    import os
-    # get config.json path
-    config_dir = os.path.abspath(__file__ + "/../../")
-    config_name = 'config.json'
-    config_path = os.path.join(config_dir, config_name)
-
-    with open(config_path, 'r') as f:
-        config = json.load(f)
-
-    return config
-
-
-config = get_config()
-chatbot = AsyncChatbot(api_key=config['openAI_key'])
-
 async def handle_response(message) -> str:
+    import os
+
+    openAI_key = os.getenv("OPENAI_KEY")
+    chatbot = AsyncChatbot(api_key=openAI_key)
     response = await chatbot.ask(message)
     responseMessage = response["choices"][0]["text"]
 
