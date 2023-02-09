a18e3c40e4a7e2d7ac76a7ae2c4d7a784b2765f5
mirtris <mirtris0522@gmail.com>
2023-02-09 15:06:44 +0800

    feat: GPT-3 model

--- src/responses.py
+++ src/responses.py
@@ -1,11 +1,18 @@
-from revChatGPT.Official import AsyncChatbot
+from asgiref.sync import sync_to_async
+import openai
 
 async def handle_response(message) -> str:
     import os
-
-    openAI_key = os.getenv("OPENAI_KEY")
-    chatbot = AsyncChatbot(api_key=openAI_key)
-    response = await chatbot.ask(message)
+    openai.api_key = os.getenv("OPENAI_KEY")
+    response = await sync_to_async(openai.Completion.create)(
+        model="text-davinci-003",
+        prompt=message,
+        temperature=0.7,
+        max_tokens=2048,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+    )
     responseMessage = response["choices"][0]["text"]
 
     return responseMessage
