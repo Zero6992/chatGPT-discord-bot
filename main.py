13ebb914214546f31d5c85cf5e134f629c4c8a04
mirtris <mirtris0522@gmail.com>
2023-02-09 15:04:13 +0800

    feat: support dotenv

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ def check_verion() -> None:
     import pkg_resources
     import src.log
 
-    # init loggger
     logger = src.log.setup_logger(__name__)
 
     # Read the requirements.txt file and add each line to a list
