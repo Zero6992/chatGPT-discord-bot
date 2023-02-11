ec18da4d62e9890732bfe90813c0049e9a8b9ad5
Mirtris <mirtris0522@gmail.com>
2023-02-11 15:02:15 +0800

    fix: support conversation

--- src/responses.py
+++ src/responses.py
@@ -1,11 +1,13 @@
 from revChatGPT.Official import AsyncChatbot
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+openAI_key = os.getenv("OPENAI_KEY")
+openAI_model = os.getenv("ENGINE")
+chatbot = AsyncChatbot(api_key=openAI_key, engine=openAI_model)
 
 async def handle_response(message) -> str:
-    import os
-
-    openAI_key = os.getenv("OPENAI_KEY")
-    openAI_model = os.getenv("ENGINE")
-    chatbot = AsyncChatbot(api_key=openAI_key, engine=openAI_model)
     response = await chatbot.ask(message)
     responseMessage = response["choices"][0]["text"]
 
