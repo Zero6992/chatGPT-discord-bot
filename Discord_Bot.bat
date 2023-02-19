430719195287b279b572f4adf530aa69b823b517
Alt3rnativity <68619138+Alt3rnativity@users.noreply.github.com>
2023-02-18 22:05:55 -0600

    Adding openai connection in the response.py

---
+++ Discord_Bot.bat
@@ -0,0 +1,4 @@
+cd C:\Repos\chatGPT-discord-bot
+powershell python3 main.py
+
+cmd /k
--- src/responses.py
+++ src/responses.py
@@ -1,14 +1,24 @@
 from revChatGPT.Official import AsyncChatbot
 from dotenv import load_dotenv
 import os
+import openai
 
 load_dotenv()
 openAI_key = os.getenv("OPENAI_KEY")
 openAI_model = os.getenv("ENGINE")
-chatbot = AsyncChatbot(api_key=openAI_key, engine=openAI_model)
+
+openai.api_key = "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"
 
 async def handle_response(message) -> str:
-    response = await chatbot.ask(message)
-    responseMessage = response["choices"][0]["text"]
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=message,
+        temperature=0.7,
+        max_tokens=2048,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+    )          
+    responseMessage = response.choices[0].text
 
     return responseMessage
