d2d547afaefd9845601c1cf1f471f98582cf25d6
mirtris <mirtris0522@gmail.com>
2023-02-26 00:05:37 +0800

    feat: session token authentication

--- src/responses.py
+++ src/responses.py
@@ -5,7 +5,8 @@ import os
 load_dotenv()
 openAI_email = os.getenv("OPENAI_EMAIL")
 openAI_password = os.getenv("OPENAI_PASSWORD")
-chatbot = AsyncChatbot(config={"email":openAI_email, "password":openAI_password})
+session_token = os.getenv("SESSION_TOKEN")
+chatbot = AsyncChatbot(config={"email":openAI_email, "password":openAI_password, "session_token":session_token})
 
 async def handle_response(message) -> str:
     async for response in chatbot.ask(message):
