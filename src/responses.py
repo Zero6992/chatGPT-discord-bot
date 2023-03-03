cd36f18682b837dd7b1b78c2b31127e428003098
Zero6992 <zero6992@Zero6992deMacBook-Pro.local>
2023-03-03 12:11:47 +0800

    feat: update new official GPT-3.5 model

--- src/responses.py
+++ src/responses.py
@@ -1,4 +1,5 @@
 from revChatGPT.V1 import AsyncChatbot
+from revChatGPT.V3 import Chatbot
 from dotenv import load_dotenv
 import os
 
@@ -6,10 +7,19 @@ load_dotenv()
 openAI_email = os.getenv("OPENAI_EMAIL")
 openAI_password = os.getenv("OPENAI_PASSWORD")
 session_token = os.getenv("SESSION_TOKEN")
-chatbot = AsyncChatbot(config={"email":openAI_email, "password":openAI_password, "session_token":session_token})
+openAI_key = os.getenv("OPENAI_API_KEY")
+openAI_engine = os.getenv("OPENAI_ENGINE")
 
-async def handle_response(message) -> str:
-    async for response in chatbot.ask(message):
+unofficial_chatbot = AsyncChatbot(config={"email":openAI_email, "password":openAI_password, "session_token":session_token})
+offical_chatbot = Chatbot(api_key=openAI_key)
+
+async def official_handle_response(message) -> str:
+    responseMessage = offical_chatbot.ask(message)
+
+    return responseMessage
+
+async def unofficial_handle_response(message) -> str:
+    async for response in unofficial_chatbot.ask(message):
         responseMessage = response["message"]
 
     return responseMessage
