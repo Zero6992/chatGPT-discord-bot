78465ad7b5b5715461db036a7068bd98a1a8ec9b
Mirtris <mirtris0522@gmail.com>
2023-03-12 15:43:32 +0800

    fix: switch model issue

--- src/responses.py
+++ src/responses.py
@@ -1,6 +1,8 @@
 from revChatGPT.V1 import AsyncChatbot
 from revChatGPT.V3 import Chatbot
 from dotenv import load_dotenv
+from typing import Union
+
 import os
 
 load_dotenv()
@@ -11,16 +13,26 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 ENGINE = os.getenv("OPENAI_ENGINE")
 CHAT_MODEL = os.getenv("CHAT_MODEL")
 
-if CHAT_MODEL ==  "UNOFFICIAL":
-    unofficial_chatbot = AsyncChatbot(config={"email":OPENAI_EMAIL, "password":OPENAI_PASSWORD, "session_token":SESSION_TOKEN})
-elif CHAT_MODEL == "OFFICIAL":
-    offical_chatbot = Chatbot(api_key=OPENAI_API_KEY, engine=ENGINE)
+
+def get_chatbot_model(model_name: str) -> Union[AsyncChatbot, Chatbot]:
+    if model_name == "UNOFFICIAL":
+        openai_email = os.getenv("OPENAI_EMAIL")
+        openai_password = os.getenv("OPENAI_PASSWORD")
+        session_token = os.getenv("SESSION_TOKEN")
+        return AsyncChatbot(config={"email": openai_email, "password": openai_password, "session_token": session_token})
+    elif model_name == "OFFICIAL":
+        openai_api_key = os.getenv("OPENAI_API_KEY")
+        print(openai_api_key)
+        engine = os.getenv("OPENAI_ENGINE")
+        return Chatbot(api_key=openai_api_key, engine=engine)
+
+chatbot = get_chatbot_model(CHAT_MODEL)
 
 async def official_handle_response(message) -> str:
-    return offical_chatbot.ask(message)
+    return chatbot.ask(message)
 
 async def unofficial_handle_response(message) -> str:
-    async for response in unofficial_chatbot.ask(message):
+    async for response in chatbot.ask(message):
         responseMessage = response["message"]
 
     return responseMessage
