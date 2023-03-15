95aeeb3809a070aa6e300adaa9a2ed087a490e13
Zero6992 <mirtris0522@gmail.com>
2023-03-15 22:06:29 +0800

    fix: gpt enigne issue

--- src/responses.py
+++ src/responses.py
@@ -11,7 +11,7 @@ OPENAI_EMAIL = os.getenv("OPENAI_EMAIL")
 OPENAI_PASSWORD = os.getenv("OPENAI_PASSWORD")
 SESSION_TOKEN = os.getenv("SESSION_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-ENGINE = os.getenv("OPENAI_ENGINE")
+ENGINE = os.getenv("GPT_ENGINE")
 CHAT_MODEL = os.getenv("CHAT_MODEL")
 
 def get_chatbot_model(model_name: str) -> Union[AsyncChatbot, Chatbot]:
@@ -22,7 +22,7 @@ def get_chatbot_model(model_name: str) -> Union[AsyncChatbot, Chatbot]:
         return AsyncChatbot(config={"email": openai_email, "password": openai_password, "session_token": session_token})
     elif model_name == "OFFICIAL":
         openai_api_key = os.getenv("OPENAI_API_KEY")
-        engine = os.getenv("OPENAI_ENGINE")
+        engine = os.getenv("GPT_ENGINE")
         return Chatbot(api_key=openai_api_key, engine=engine)
 
 chatbot = get_chatbot_model(CHAT_MODEL)
