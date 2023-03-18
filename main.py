aa132d5f87a304dfa366e9788f4cc68c9731789b
Mirtris <mirtris0522@gmail.com>
2023-03-19 03:23:13 +0800

    fix: typo

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ def check_version() -> None:
     # For each library listed in requirements.txt, check if the corresponding version is installed
     for package in required:
         # Use the pkg_resources library to get information about the installed version of the library
-        package_name, package_verion = package.split('==')
+        package_name, package_version = package.split('==')
         installed = pkg_resources.get_distribution(package_name)
         # Extract the library name and version number
         name, version = installed.project_name, installed.version
         # Compare the version number to see if it matches the one in requirements.txt
         if package != f'{name}=={version}':
             logger.error(f'{name} version {version} is installed but does not match the requirements')
-            sys.exit();
+            sys.exit()
 
 if __name__ == '__main__': 
     check_version()
