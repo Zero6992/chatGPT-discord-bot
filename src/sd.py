36f9acc9e902f25415387994e1a8adbd906c1540
XiuyuanLu <276908010@qq.com>
2023-03-24 20:48:35 +0800

    fix sd config

--- src/sd.py
+++ src/sd.py
@@ -14,7 +14,7 @@ load_dotenv()
 stability_api = client.StabilityInference(
     key=os.environ['STABILITY_KEY'], # API Key reference.
     verbose=True, # Print debug messages.
-    engine="stable-diffusion-v1-5", # Set the engine to use for generation.
+    engine=os.environ['STABILITY_ENGINE'], # Set the engine to use for generation.
     # Available engines: stable-diffusion-v1 stable-diffusion-v1-5 stable-diffusion-512-v2-0 stable-diffusion-768-v2-0
     # stable-diffusion-512-v2-1 stable-diffusion-768-v2-1 stable-inpainting-v1-0 stable-inpainting-512-v2-0
 )
