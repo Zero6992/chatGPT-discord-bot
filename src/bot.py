2868d48fcd66d5916f689ac1be8525e800a10c04
Zoltan Kiss5 <Zoltan_Kiss5@epam.com>
2023-05-09 16:40:04 +0200

    Remove persona and random 2

--- src/bot.py
+++ src/bot.py
@@ -205,13 +205,13 @@ def run_discord_bot():
         # - `/draw [prompt]` Generate an image with the Dalle2 model
         # - `/switchpersona [persona]` Switch between optional ChatGPT jailbreaks
                 # `random`: Picks a random persona
-                `chatgpt`: Standard ChatGPT mode
-                `dan`: Dan Mode 11.0, infamous Do Anything Now Mode
-                `sda`: Superior DAN has even more freedom in DAN Mode
-                `confidant`: Evil Confidant, evil trusted confidant
-                `based`: BasedGPT v2, sexy GPT
-                `oppo`: OPPO says exact opposite of what ChatGPT would say
-                `dev`: Developer Mode, v2 Developer mode enabled
+                # `chatgpt`: Standard ChatGPT mode
+                # `dan`: Dan Mode 11.0, infamous Do Anything Now Mode
+                # `sda`: Superior DAN has even more freedom in DAN Mode
+                # `confidant`: Evil Confidant, evil trusted confidant
+                # `based`: BasedGPT v2, sexy GPT
+                # `oppo`: OPPO says exact opposite of what ChatGPT would say
+                # `dev`: Developer Mode, v2 Developer mode enabled
 
         - `/private` ChatGPT switch to private mode
         - `/public` ChatGPT switch to public mode
