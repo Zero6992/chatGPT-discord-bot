a0ef0d9641ab5b8815b686ed9c8429c149caca70
Zero6992 <mirtris0522@gmail.com>
2023-06-29 18:05:31 +0800

    chore: modify code organization

--- src/aclient.py
+++ src/aclient.py
@@ -3,23 +3,21 @@ import json
 import discord
 import asyncio
 from typing import Union
+
 from src import responses
 from src.log import logger
+from utils.message_utils import send_split_message
+from auto_login.AutoLogin import GoogleBardAutoLogin, MicrosoftBingAutoLogin
+
 from dotenv import load_dotenv
 from discord import app_commands
+
 from revChatGPT.V3 import Chatbot
 from revChatGPT.V1 import AsyncChatbot
 from Bard import Chatbot as BardChatbot
 from EdgeGPT.EdgeGPT import Chatbot as EdgeChatbot
-from auto_login.AutoLogin import GoogleBardAutoLogin, MicrosoftBingAutoLogin
 
 load_dotenv()
-
-config_dir = os.path.abspath(f"{__file__}/../../")
-prompt_name = 'system_prompt.txt'
-prompt_path = os.path.join(config_dir, prompt_name)
-with open(prompt_path, "r", encoding="utf-8") as f:
-    prompt = f.read()
 
 class aclient(discord.Client):
     def __init__(self) -> None:
@@ -56,11 +54,17 @@ class aclient(discord.Client):
             bing_password = os.getenv("bing_password")
             MicrosoftBingAutoLogin(bing_account, bing_password, chrome_version).dump_cookies()
 
+        config_dir = os.path.abspath(f"{__file__}/../../")
+        prompt_name = 'system_prompt.txt'
+        prompt_path = os.path.join(config_dir, prompt_name)
+        with open(prompt_path, "r", encoding="utf-8") as f:
+            self.starting_prompt = f.read()
+
         self.chat_model = os.getenv("CHAT_MODEL")
         self.chatbot = self.get_chatbot_model()
         self.message_queue = asyncio.Queue()
 
-    def get_chatbot_model(self, prompt=prompt) -> Union[AsyncChatbot, Chatbot]:
+    def get_chatbot_model(self) -> Union[AsyncChatbot, Chatbot]:
         if self.chat_model == "UNOFFICIAL":
             return AsyncChatbot(config = {
                 "access_token": self.chatgpt_access_token,
@@ -68,7 +72,7 @@ class aclient(discord.Client):
                 "PUID": self.chatgpt_paid
             })
         elif self.chat_model == "OFFICIAL":
-                return Chatbot(api_key=self.openAI_API_key, engine=self.openAI_gpt_engine, system_prompt=prompt)
+                return Chatbot(api_key=self.openAI_API_key, engine=self.openAI_gpt_engine, system_prompt=self.starting_prompt)
         elif self.chat_model == "Bard":
             return BardChatbot(session_id=self.bard_session_id)
         elif self.chat_model == "Bing":
@@ -109,90 +113,36 @@ class aclient(discord.Client):
                 response = f"{response}{await responses.bard_handle_response(user_message, self)}"
             elif self.chat_model == "Bing":
                 response = f"{response}{await responses.bing_handle_response(user_message, self)}"
-            char_limit = 1900
-            if len(response) > char_limit:
-                # Split the response into smaller chunks of no more than 1900 characters each(Discord limit is 2000 per chunk)
-                if "```" in response:
-                    # Split the response if the code block exists
-                    parts = response.split("```")
-
-                    for i in range(len(parts)):
-                        if i%2 == 0: # indices that are even are not code blocks
-                            if self.is_replying_all == "True":
-                                await message.channel.send(parts[i])
-                            else:
-                                await message.followup.send(parts[i])
-                        else: # Odd-numbered parts are code blocks
-                            code_block = parts[i].split("\n")
-                            formatted_code_block = ""
-                            for line in code_block:
-                                while len(line) > char_limit:
-                                    # Split the line at the 50th character
-                                    formatted_code_block += line[:char_limit] + "\n"
-                                    line = line[char_limit:]
-                                formatted_code_block += line + "\n"  # Add the line and seperate with new line
-
-                            # Send the code block in a separate message
-                            if (len(formatted_code_block) > char_limit+100):
-                                code_block_chunks = [formatted_code_block[i:i+char_limit]
-                                                    for i in range(0, len(formatted_code_block), char_limit)]
-                                for chunk in code_block_chunks:
-                                    if self.is_replying_all == "True":
-                                        await message.channel.send(f"```{chunk}```")
-                                    else:
-                                        await message.followup.send(f"```{chunk}```")
-                            elif self.is_replying_all == "True":
-                                await message.channel.send(f"```{formatted_code_block}```")
-                            else:
-                                await message.followup.send(f"```{formatted_code_block}```")
-                else:
-                    response_chunks = [response[i:i+char_limit]
-                                    for i in range(0, len(response), char_limit)]
-                    for chunk in response_chunks:
-                        if self.is_replying_all == "True":
-                            await message.channel.send(chunk)
-                        else:
-                            await message.followup.send(chunk)
-            elif self.is_replying_all == "True":
-                await message.channel.send(response)
-            else:
-                await message.followup.send(response)
+            await send_split_message(self, response, message)
         except Exception as e:
+            logger.exception(f"Error while sending : {e}")
             if self.is_replying_all == "True":
                 await message.channel.send(f"> **ERROR: Something went wrong, please try again later!** \n ```ERROR MESSAGE: {e}```")
             else:
                 await message.followup.send(f"> **ERROR: Something went wrong, please try again later!** \n ```ERROR MESSAGE: {e}```")
-            logger.exception(f"Error while sending message: {e}")
 
     async def send_start_prompt(self):
-        import os.path
-
-        config_dir = os.path.abspath(f"{__file__}/../../")
-        prompt_name = 'system_prompt.txt'
-        prompt_path = os.path.join(config_dir, prompt_name)
         discord_channel_id = os.getenv("DISCORD_CHANNEL_ID")
         try:
-            if os.path.isfile(prompt_path) and os.path.getsize(prompt_path) > 0:
-                with open(prompt_path, "r", encoding="utf-8") as f:
-                    prompt = f.read()
-                    if (discord_channel_id):
-                        logger.info(f"Send system prompt with size {len(prompt)}")
-                        response = ""
-                        if self.chat_model == "OFFICIAL":
-                            response = f"{response}{await responses.official_handle_response(prompt, self)}"
-                        elif self.chat_model == "UNOFFICIAL":
-                            response = f"{response}{await responses.unofficial_handle_response(prompt, self)}"
-                        elif self.chat_model == "Bard":
-                            response = f"{response}{await responses.bard_handle_response(prompt, self)}"
-                        elif self.chat_model == "Bing":
-                            response = f"{response}{await responses.bing_handle_response(prompt, self)}"
-                        channel = self.get_channel(int(discord_channel_id))
-                        await channel.send(response)
-                        logger.info(f"System prompt response:{response}")
-                    else:
-                        logger.info("No Channel selected. Skip sending system prompt.")
+            if self.starting_prompt:
+                if (discord_channel_id):
+                    logger.info(f"Send system prompt with size {len(self.starting_prompt)}")
+                    response = ""
+                    if self.chat_model == "OFFICIAL":
+                        response = f"{response}{await responses.official_handle_response(self.starting_prompt, self)}"
+                    elif self.chat_model == "UNOFFICIAL":
+                        response = f"{response}{await responses.unofficial_handle_response(self.starting_prompt, self)}"
+                    elif self.chat_model == "Bard":
+                        response = f"{response}{await responses.bard_handle_response(self.starting_prompt, self)}"
+                    elif self.chat_model == "Bing":
+                        response = f"{response}{await responses.bing_handle_response(self.starting_prompt, self)}"
+                    channel = self.get_channel(int(discord_channel_id))
+                    await channel.send(response)
+                    logger.info(f"System prompt response:{response}")
+                else:
+                    logger.info("No Channel selected. Skip sending system prompt.")
             else:
-                logger.info(f"No {prompt_name}. Skip sending system prompt.")
+                logger.info(f"Not given starting prompt. Skiping...")
         except Exception as e:
             logger.exception(f"Error while sending system prompt: {e}")
 
