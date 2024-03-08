f570427aed31d15a160bf6285714fa78972fd691
Zero6992 <mirtris0522@gmail.com>
2024-03-08 22:13:17 +0800

    feat: imitate feature

--- src/art.py
+++ src/art.py
@@ -9,3 +9,11 @@ async def draw(model: str, prompt: str) -> str:
     image_url = response.data[0].url
 
     return image_url
+
+
+async def imitate(model: str, image) -> str:
+    async_generate = sync_to_async(g4f_client.images.create_variation, thread_sensitive=True)
+    response = await async_generate(model = model, image = image)
+    image_url = response.data[0].url
+
+    return image_url
